06623da430666946c78dc7c85eaa4d929d61f5d6
Bmar21 <brandonmalor21@gmail.com>
2016-09-29 12:23:50 -0600

    I added a try catch method

--- src/popups/controller/PopUpsController.java
+++ src/popups/controller/PopUpsController.java
@@ -22,5 +22,30 @@ public class PopUpsController
 			
 		}
 	}
+	
+	/** 
+	 * Checks a supplied String value to see if can be converted to a double.
+	 * @param potentialValue The supplied String
+	 * @return Whether the conversion to a double is possible as a boolean value. 
+	 */
+	private boolean isDouble(String potentialValue)
+	{
+		boolean vaildDouble = false;
+		
+		try
+		{
+			double testValue = Double.parseDouble(potentialValue); 
+			validDouble = true;
+		}
+		catch(NumberFormatException notDoubleError)
+		{
+			display.displayMessage("That was not s double =:<");
+		}
+		
+		return vaildDouble;
+	}
+
+
+
 
 }
